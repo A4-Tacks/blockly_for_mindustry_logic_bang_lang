14465fae5b56ff469989579a698471d196fdf378
A4-Tacks <wdsjxhno1001@163.com>
2023-12-26 05:11:17 +0800

    添加块数据的导入及导出
    - 变更了存储键, 这会导致浏览器本地存储的编辑进度丢失

--- src/serialization.js
+++ src/serialization.js
@@ -7,7 +7,7 @@
 import * as Blockly from 'blockly/core';
 
 // Use a unique storage key for this codelab
-const storageKey = 'jsonGeneratorWorkspace';
+const storageKey = 'bangGeneratorWorkspace';
 
 /**
  * Saves the state of the workspace to browser's local storage.
@@ -26,8 +26,12 @@ export const load = function(workspace) {
   const data = window.localStorage?.getItem(storageKey);
   if (!data) return;
 
+  loadObj(JSON.parse(data), workspace)
+};
+
+export const loadObj = function(obj, workspace) {
   // Don't emit events during loading.
   Blockly.Events.disable();
-  Blockly.serialization.workspaces.load(JSON.parse(data), workspace, false);
+  Blockly.serialization.workspaces.load(obj, workspace, false);
   Blockly.Events.enable();
 };
